29e4e199c5be6506f4c38539503bc1e977352a57
unoValencia <valenciajg@students.nu-lipa.edu.ph>
2024-06-18 23:02:07 +0800

    Add files via upload
    this is it

--- cakes.sql
+++ cakes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 16, 2024 at 03:21 PM
+-- Generation Time: Jun 18, 2024 at 05:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -35,18 +35,21 @@ CREATE TABLE `registered_user` (
   `birthday` date NOT NULL,
   `sex` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `user_profile_picture` varchar(255) NOT NULL
+  `user_profile_picture` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `account_type` int(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `registered_user`
 --
 
-INSERT INTO `registered_user` (`registered_Id`, `username`, `firstname`, `lastname`, `birthday`, `sex`, `email`, `user_profile_picture`) VALUES
-(1, 'Gloria', 'Gloria', 'Macapagal', '2001-01-01', 'Male', 'gloria@gmail.com', 'uploads/3490883874_08a361fec8_b_1718543599.jpg'),
-(2, 'Gloria', 'Gloria', 'Macapagal', '2001-01-01', 'Male', 'gloria@gmail.com', 'uploads/3490883874_08a361fec8_b_1718543821.jpg'),
-(3, 'Gloria', 'Gloria', 'Macapagal', '2001-01-01', 'Male', 'gloria@gmail.com', 'uploads/3490883874_08a361fec8_b_1718543886.jpg'),
-(4, 'Gloria', 'Gloria', 'Macapagal', '2001-01-01', 'Male', 'gloria@gmail.com', 'uploads/3490883874_08a361fec8_b_1718543909.jpg');
+INSERT INTO `registered_user` (`registered_Id`, `username`, `firstname`, `lastname`, `birthday`, `sex`, `email`, `user_profile_picture`, `password`, `account_type`) VALUES
+(5, 'unorawr', 'Uno', 'Valencia', '2024-06-17', 'Female', 'janinevalencian27@gmail.com', 'uploads/436786100_958613725511022_3871309330666312434_n.jpg', NULL, 0),
+(6, 'unorawr', 'Uno', 'Valencia', '2024-06-17', 'Female', 'janinevalencian27@gmail.com', 'uploads/436786100_958613725511022_3871309330666312434_n_1718628756.jpg', NULL, 0),
+(7, 'unorawr', 'Uno', 'Valencia', '2024-06-17', 'Female', 'janinevalencian27@gmail.com', 'uploads/436786100_958613725511022_3871309330666312434_n_1718628760.jpg', NULL, 0),
+(8, 'unorawr', 'Uno', 'Valencia', '2024-06-13', 'Male', 'janinevalencian27@gmail.com', 'uploads/439755772_347062628375651_6956481040210101511_n.jpg', NULL, 0),
+(17, 'user', 'Uno', 'Valencia', '2024-06-18', 'Male', 'user@gmail.com', 'uploads/C-lx2EFXYAEvE68_1718719904.jpg', '$2y$10$e.jA.H8lWCPf4IXBsxKGk.1rOggKJELjv18Lo9SEnLLOX5JGwqeWC', 0);
 
 -- --------------------------------------------------------
 
@@ -59,16 +62,20 @@ CREATE TABLE `signup` (
   `account_type` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `user_profile_picture` int(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `signup`
 --
 
-INSERT INTO `signup` (`User_Id`, `account_type`, `username`, `email`, `password`) VALUES
-(1, 0, 'Ivan', 'ivan@gmail.com', 'Ivanmalaki'),
-(2, 0, 'Gloria', 'glory@gmail.com', '$2y$10$VK2gmATL1ggtJ4pwtuqjSuzIpt3LdOC6sOYu5/SWOstF6u/hM4LZy');
+INSERT INTO `signup` (`User_Id`, `account_type`, `username`, `email`, `password`, `user_profile_picture`) VALUES
+(1, 0, 'Ivan', 'ivan@gmail.com', 'Ivanmalaki', NULL),
+(2, 0, 'Gloria', 'glory@gmail.com', '$2y$10$VK2gmATL1ggtJ4pwtuqjSuzIpt3LdOC6sOYu5/SWOstF6u/hM4LZy', NULL),
+(3, 0, 'uno', 'janinevalencian27@gmail.com', '$2y$10$OFQToxGLihy7HhBJIeQPxOs5M2ZKDCRjujxA1yGK/v1BRGJ6d/sYe', NULL),
+(4, 0, 'unoo', 'janinevalencian27@gmail.com', '$2y$10$vTromPVtoJO97KMVPWPT5.DX6ByCbCeYf1bkySZ6AafA21WcBYj56', NULL),
+(8, 0, 'Uno', 'Valencia', '2024-06-18', 0);
 
 -- --------------------------------------------------------
 
@@ -81,7 +88,8 @@ CREATE TABLE `user_address` (
   `street` varchar(255) NOT NULL,
   `barangay` varchar(255) NOT NULL,
   `city` varchar(255) NOT NULL,
-  `province` varchar(255) NOT NULL
+  `province` varchar(255) NOT NULL,
+  `registered_Id` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -104,7 +112,8 @@ ALTER TABLE `signup`
 -- Indexes for table `user_address`
 --
 ALTER TABLE `user_address`
-  ADD PRIMARY KEY (`user_add_id`);
+  ADD PRIMARY KEY (`user_add_id`),
+  ADD KEY `registered_Id` (`registered_Id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -114,19 +123,29 @@ ALTER TABLE `user_address`
 -- AUTO_INCREMENT for table `registered_user`
 --
 ALTER TABLE `registered_user`
-  MODIFY `registered_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `registered_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `signup`
 --
 ALTER TABLE `signup`
-  MODIFY `User_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `User_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user_address`
 --
 ALTER TABLE `user_address`
   MODIFY `user_add_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_address`
+--
+ALTER TABLE `user_address`
+  ADD CONSTRAINT `user_address_ibfk_1` FOREIGN KEY (`registered_Id`) REFERENCES `registered_user` (`registered_Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
